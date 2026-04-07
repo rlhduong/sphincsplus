bd3e83f4465409252035a0e6d1043754a7343a9d
Roger <rogerduonglonghieu@gmail.com>
2026-04-08 09:18:57 +1000

    params and address

---
+++ src/address.py
@@ -0,0 +1,122 @@
+import struct
+from enum import IntEnum
+
+
+class AdrsType(IntEnum):
+    WOTS_HASH  = 0
+    WOTS_PK    = 1
+    TREE       = 2
+    FORS_TREE  = 3
+    FORS_ROOTS = 4
+    WOTS_PRF   = 5
+    FORS_PRF   = 6
+
+
+_LAYER  = slice(0,  4)      # layer address
+_TREE   = slice(4,  16)     # tree address (64-bit big-endian)
+_TYPE   = slice(16, 20)     # type
+_WORD1  = slice(20, 24)     # keypair
+_WORD2  = slice(24, 28)     # chain / tree-height
+_WORD3  = slice(28, 32)     # hash  / tree-index
+
+
+class ADRS:
+    layer: bytearray
+    tree: bytearray
+    type: int
+    key_pair: bytearray
+    chain: bytearray
+    hash: bytearray
+    tree_index: bytearray
+    tree_height: bytearray
+    def __init__(self):
+        self.layer = bytearray(4)
+        self.tree  = bytearray(12)
+        self.type  = 0
+    
+        self.key_pair = bytearray(4)
+        self.chain = bytearray(4)
+        self.hash = bytearray(4)
+    
+        self.tree_index = bytearray(4)
+        self.tree_height = bytearray(4)
+
+    def copy(self) -> 'ADRS':
+        copy = ADRS()
+        copy.layer[:] = self.layer
+        copy.tree[:] = self.tree
+        copy.type = self.type
+        copy.key_pair[:] = self.key_pair
+        copy.chain[:] = self.chain
+        copy.hash[:] = self.hash
+        copy.tree_index[:] = self.tree_index
+        copy.tree_height[:] = self.tree_height
+        return copy
+    
+    def to_bytes(self) -> bytes:
+        ADRSc = bytearray(32)
+        ADRSc[_LAYER] = self.layer
+        ADRSc[_TREE] = self.tree
+        ADRSc[_TYPE] = struct.pack('>I', self.type)
+
+        if self.type in AdrsType.WOTS_HASH:
+            ADRSc[_WORD1] = self.key_pair
+            ADRSc[_WORD2] = self.chain
+            ADRSc[_WORD3] = self.hash
+        elif self.type in AdrsType.WOTS_PK:
+            ADRSc[_WORD1] = self.key_pair
+        elif self.type in AdrsType.TREE:
+            ADRSc[_WORD2] = self.tree_height
+            ADRSc[_WORD3] = self.tree_index
+        elif self.type in AdrsType.FORS_TREE:
+            ADRSc[_WORD1] = self.key_pair
+            ADRSc[_WORD2] = self.tree_height
+            ADRSc[_WORD3] = self.tree_index
+        elif self.type in AdrsType.FORS_ROOTS:
+            ADRSc[_WORD1] = self.key_pair
+        elif self.type in AdrsType.WOTS_PRF:
+            ADRSc[_WORD1] = self.key_pair
+            ADRSc[_WORD2] = self.chain
+        elif self.type in AdrsType.FORS_PRF:
+            ADRSc[_WORD1] = self.key_pair
+            ADRSc[_WORD3] = self.tree_index
+
+        return bytes(ADRSc)
+    
+    def set_layer(self, layer: int):
+        self.layer = struct.pack('>I', layer)
+
+    def set_tree(self, tree: int):
+        self.tree = struct.pack('>Q', tree)
+
+    def set_type(self, type: AdrsType):
+        self.type = type
+        self.key_pair = bytearray(4)
+        self.chain = bytearray(4)
+        self.hash = bytearray(4)
+        self.tree_index = bytearray(4)
+        self.tree_height = bytearray(4)
+
+    def set_key_pair(self, key_pair: int):
+        self.key_pair = struct.pack('>I', key_pair)
+
+    def set_chain(self, chain: int):
+        self.chain = struct.pack('>I', chain)  
+
+    def set_hash(self, hash: int):
+        self.hash = struct.pack('>I', hash)
+    
+    def set_tree_index(self, tree_index: int):
+        self.tree_index = struct.pack('>I', tree_index)
+
+    def set_tree_height(self, tree_height: int):
+        self.tree_height = struct.pack('>I', tree_height)
+
+    def __str__(self) -> str:
+        return f"ADRS(layer={self.layer.hex()}, tree={self.tree.hex()}, type={self.type}, key_pair={self.key_pair.hex()}, chain={self.chain.hex()}, hash={self.hash.hex()}, tree_index={self.tree_index.hex()}, tree_height={self.tree_height.hex()})"
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+
+        
---
+++ src/parameters.py
@@ -0,0 +1,85 @@
+"""
+Parameter set naming convention:
+    sphincs-{hash}-{n*8}{s|f}
+        hash : sha2 | shake
+        n*8  : 128 | 192 | 256   (security level in bits)
+        s|f  : small (few sigs, small keys) | fast (many sigs, fast signing)
+
+name: sphincs-{hash}-{n*8}{s|f}
+n : the security parameter in bytes.
+w : the Winternitz parameter.
+h : the height of the hypertree.
+d : the number of layers in the hypertree.
+k : the number of trees in FORS.
+t : the number of leaves of a FORS tree.
+a : the height of a FORS tree. log2(t)
+m : the message digest length (bytes)
+hash    : "sha2" or "shake"
+"""
+
+from hashlib import sha256, shake_256
+from typing import Callable
+import math
+
+def sha256_digest(data: bytes) -> bytes:
+    return sha256(data).digest()
+
+def shake256_digest(n: int):
+    def _hash(data: bytes) -> bytes:
+        return shake_256(data).digest(n)
+    return _hash
+
+class Parameters:
+    default_parameters = {
+        "sphincs-sha2-128s": (16, 16, 63, 7, 14, 12, sha256_digest),
+        "sphincs-sha2-128f": (16, 16, 66, 22, 33, 6, sha256_digest),
+        "sphincs-sha2-192s": (24, 16, 63, 7, 17, 14, sha256_digest),
+        "sphincs-sha2-192f": (24, 16, 66, 22, 33, 8, sha256_digest),
+        "sphincs-sha2-256s": (32, 16, 64, 8, 22, 14, sha256_digest),
+        "sphincs-sha2-256f": (32, 16, 68, 17, 35, 9, sha256_digest),
+
+        "sphincs-shake-128s": (16, 16, 63, 7, 14, 12, shake256_digest(16)),
+        "sphincs-shake-128f": (16, 16, 66, 22, 33, 6, shake256_digest(16)),
+        "sphincs-shake-192s": (24, 16, 63, 7, 17, 14, shake256_digest(24)),
+        "sphincs-shake-192f": (24, 16, 66, 22, 33, 8, shake256_digest(24)),
+        "sphincs-shake-256s": (32, 16, 64, 8, 22, 14, shake256_digest(32)),
+        "sphincs-shake-256f": (32, 16, 68, 17, 35, 9, shake256_digest(32)),
+    }
+        
+    def __init__(self, n: int, w: int, h: int, d: int, k: int, log_t: int, hash_fn: Callable[[bytes], bytes]):
+        self.n = n
+        self.w = w
+        self.h = h
+        self.d = d
+        self.k = k
+        self.log_t = log_t
+        self.a = log_t
+        self.hash_fn = hash_fn
+        self.log_w = int(math.log2(w))
+
+    @property
+    def len1(self) -> int:
+        # ceil((8 * n) / log_w)
+        return math.ceil((8 * self.n) / self.log_w)
+
+    @property
+    def len2(self) -> int:
+        # floor(log_w(len1 * (w - 1))) + 1
+        return math.floor(math.log2(self.len1 * (self.w - 1)) / self.log_w) + 1
+
+    @property
+    def wots_len(self) -> int:
+        return self.len1 + self.len2
+
+    @property
+    def m(self) -> int:
+        return math.floor((self.k * self.log_t + 7) / 8) + math.floor((self.h - self.h / self.d + 7) / 8) + math.floor((self.h / self.d + 7) / 8)
+    
+    @classmethod
+    def get_paramset(cls, name: str) -> 'Parameters':
+        n, w, h, d, k, log_t, hash_fn = cls.default_parameters[name]
+        return Parameters(n, w, h, d, k, log_t, hash_fn)
+    
+    def __str__(self) -> str:
+        return f"Parameters(n={self.n}, w={self.w}, h={self.h}, d={self.d}, k={self.k}, log_t={self.log_t})"
+    
